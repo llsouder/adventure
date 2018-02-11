00333914960a403608d368cc1c4873f4ff5333e6
Lonnie Souder <lonnie@souderfamily.net>
2018-02-11 06:34:18 -0500

    Draw a red square test.

--- src/cljs/adventure/core.cljs
+++ src/cljs/adventure/core.cljs
@@ -5,11 +5,19 @@
             [adventure.views :as views]
             [adventure.config :as config]))
 
+(def canvas (.getElementById js/document "drawing1" ))
+(def ctx (.getContext canvas "2d"))
+
 
 (defn dev-setup []
   (when config/debug?
     (enable-console-print!)
     (println "dev mode")))
+
+(set! (.-fillStyle ctx) "rgb(255,0,0)")
+(.beginPath ctx)
+(.rect ctx 100 100 50 30)
+(.fill ctx)
 
 (defn mount-root []
   (re-frame/clear-subscription-cache!)
@@ -20,3 +28,4 @@
   (re-frame/dispatch-sync [::events/initialize-db])
   (dev-setup)
   (mount-root))
+ 
